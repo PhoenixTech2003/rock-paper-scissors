b09ff7423f8ee82e8bcbfaaac4e7972b35de6293
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com.github.com>
2023-08-04 18:58:15 +0200

    Create game function in code.js

--- js/code.js
+++ js/code.js
@@ -1,10 +1,13 @@
 function getcomputerchoice() {
+    // generate random number
     let choice = Math.floor(Math.random() * 3);
-
+    // if choice is 1 pick rock
     if (choice === 1) {
         return "rock";
+    // if choice is 2 pick scissors
     }else if (choice === 2){
         return "scissors";
+    //if choce is anything else between 0 and 3 but not 1 or 2 pick paper
     }else {
         return "paper";
     }
@@ -24,14 +27,14 @@ function playround(computerselection, playerselection){
     // if computerselection = scissors and lowercaseplayerdelection = rock
     if (computerselection === "scissors" && lowercaseplayerdelection === "rock"){
         // player wins
-        return `You win ${lowercaseplayerdelection} beats ${computerselection}`;
+        return 1;
 
     }
 
     // if computerselection = rock and lowercaseplayerdelection = paper
     if (computerselection === "rock" && lowercaseplayerdelection === "paper"){
         // player wins
-        return `You win ${lowercaseplayerdelection} beats ${computerselection}`; 
+        return 1; 
 
     }
 
@@ -39,21 +42,21 @@ function playround(computerselection, playerselection){
     // if computerselection = paper and lowercaseplayerdelection = rock
     if (computerselection === "paper" && lowercaseplayerdelection === "rock"){
         // computer wins
-        return `You lose ${computerselection} beats ${lowercaseplayerdelection}`;
+        return 0;
 
     }
 
     // if computerselection = scissors and lowercaseplayerdelection = paper
     if (computerselection === "scissors" && lowercaseplayerdelection === "paper"){
         // computer wins
-        return `You lose ${computerselection} beats ${lowercaseplayerdelection}`;
+        return 0;
 
     }
 
     // if computerselection = paper and lowercaseplayerdelection = scissors
     if (computerselection === "paper" && lowercaseplayerdelection === "scissors"){
         // player wins
-        return `You win ${lowercaseplayerdelection} beats ${computerselection}`;
+        return 1;
 
     }
 
@@ -65,9 +68,56 @@ function playround(computerselection, playerselection){
     }
 }
 
-const computerselection = getcomputerchoice();
 
-const playerselection = "rock";
+function game (){
+    //create variable for computer score and player score
+    let computerscore = 0;
+    let playerscore = 0;
+    //create loop trackerr
+    let count = 0;
+  //create loop that plays for four rounds
+    while (count <= 4){
+        //call computer choice function
+        const computerselection = getcomputerchoice();
+        //prompt for user input
+        const playerselection = prompt("rock, paper, scissors?");
 
-result = playround(computerselection,playerselection)
-console.log(result)
+        //varible keeping  winner
+        let verifywinner = playround(computerselection,playerselection);
+        //verify winner
+        if (verifywinner === 0){
+            alert(`You lose ${computerselection} beats ${playerselection}`);
+            //increase computer score
+            computerscore ++;
+
+        }else if (verifywinner === 1){
+            alert(`You Win ${computerselection} beats ${playerselection}`);
+            //increase playerscore
+            playerscore ++;
+        }else{
+            alert(`It's a draw`)
+            playerscore ++;
+            computerscore ++;
+        }
+        
+        //increment loop tracker by one
+        count ++;
+    }
+
+    //determine winner
+    if (computerscore > playerscore){
+        alert(`hahahah You lose I have ${computerscore} points and you have ${playerscore} points`);
+    }
+
+    if (playerscore > computerscore){
+        alert(`Oh snap You win I have ${computerscore} points and you have ${playerscore} points`);
+    }
+
+    if (playerscore === computerscore){
+        alert("It's a draw");
+    }
+}
+
+
+// call game function
+let playgame = game();
