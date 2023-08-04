5ab6bf44b70e310ab40b6d10e118fb0a0210aa35
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com.github.com>
2023-08-04 13:11:56 +0200

    Create playround function in code.js

--- js/code.js
+++ js/code.js
@@ -10,4 +10,64 @@ function getcomputerchoice() {
     }
 }
 
-console.log(getcomputerchoice())
+function playround(computerselection, playerselection){
+    //convert lowercaseplayerdelection to lowercase
+    let lowercaseplayerdelection = playerselection.toLowerCase()
+    //if computerselection = rock and lowercaseplayerdelection = scissors
+    if (computerselection === "rock" && lowercaseplayerdelection === "scissors"){
+        // computer wins
+        return `You lose ${computerselection} beats ${lowercaseplayerdelection}`;
+
+    }
+        
+
+    // if computerselection = scissors and lowercaseplayerdelection = rock
+    if (computerselection === "scissors" && lowercaseplayerdelection === "rock"){
+        // player wins
+        return `You win ${lowercaseplayerdelection} beats ${computerselection}`;
+
+    }
+
+    // if computerselection = rock and lowercaseplayerdelection = paper
+    if (computerselection === "rock" && lowercaseplayerdelection === "paper"){
+        // player wins
+        return `You win ${lowercaseplayerdelection} beats ${computerselection}`; 
+
+    }
+
+
+    // if computerselection = paper and lowercaseplayerdelection = rock
+    if (computerselection === "paper" && lowercaseplayerdelection === "rock"){
+        // computer wins
+        return `You lose ${computerselection} beats ${lowercaseplayerdelection}`;
+
+    }
+
+    // if computerselection = scissors and lowercaseplayerdelection = paper
+    if (computerselection === "scissors" && lowercaseplayerdelection === "paper"){
+        // computer wins
+        return `You lose ${computerselection} beats ${lowercaseplayerdelection}`;
+
+    }
+
+    // if computerselection = paper and lowercaseplayerdelection = scissors
+    if (computerselection === "paper" && lowercaseplayerdelection === "scissors"){
+        // player wins
+        return `You win ${lowercaseplayerdelection} beats ${computerselection}`;
+
+    }
+
+    // if computerselection === lowercaseplayerdelection
+    if (computerselection === lowercaseplayerdelection){
+        // draw
+        return "Draw";
+
+    }
+}
+
+const computerselection = getcomputerchoice();
+
+const playerselection = "rock";
+
+result = playround(computerselection,playerselection)
+console.log(result)
